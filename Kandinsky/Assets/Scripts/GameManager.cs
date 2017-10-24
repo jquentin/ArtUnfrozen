c33cee806d0a0d2eec3ebda99a8641fd8aec262a
Jeremy Quentin <jquentin@Jeremys-MacBook-Pro.local>
2017-10-24 22:36:42 +0800

    Import Kandinsky

---
+++ Kandinsky/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	[ContextMenu("InitOrderInLayer")]
+	public void InitOrderInLayer () 
+	{
+		int orderIndex = 0;
+		IEnumerable<Planet> planets = FindObjectsOfType<Planet>().OrderByDescending((Planet arg) => arg.size);
+		foreach(Planet p in planets)
+		{
+			p.orderInLayer = orderIndex;
+			orderIndex += 10;
+		}
+	}
+
+}
---
+++ Kandinsky/Assets/Scripts/Planet.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+	public float size
+	{
+		get
+		{
+			return GetComponentsInChildren<SpriteRenderer>().Min((SpriteRenderer arg) => arg.bounds.size.magnitude);
+		}
+	}
+
+	public int orderInLayer
+	{
+		set
+		{
+			int index = value;
+			foreach(SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+			{
+				if (sr.gameObject != this.gameObject)
+				{
+					sr.sortingOrder = index;
+					index++;
+				}
+			}
+			GetComponent<SpriteRenderer>().sortingOrder = index;
+		}
+	}
+
+}
