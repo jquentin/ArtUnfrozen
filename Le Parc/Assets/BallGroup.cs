c4cf3e083033b9fed09dbb7e209944a2357d3fb1
Jeremy Quentin <jeremy.quentin@gmail.com>
2019-12-16 09:41:33 +0100

    Add Le Parc

---
+++ Le Parc/Assets/BallGroup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using DG.Tweening;
+
+public class BallGroup : MonoBehaviour
+{
+	[SerializeField] float speed = 1f;
+
+	void Update()
+	{
+		transform.position += new Vector3(0f, Time.deltaTime * speed, 0f);
+		List<TrailRenderer> orderedTrails = GetComponentsInChildren<TrailRenderer>().OrderByDescending(r => r.transform.position.y).ToList();
+		for (int i = 0; i < orderedTrails.Count; i++)
+		{
+			orderedTrails[i].sortingOrder = i;
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Turn(true);
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Turn(false);
+		}
+	}
+
+	void Turn(bool left)
+	{
+		transform.DORotate((left ? 1f : -1f) * Vector3.fwd * 45f, 0.5f).SetRelative(true);
+	}
+}
---
+++ Le Parc/Assets/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+
+	[SerializeField] Transform target;
+
+	Vector3 offset;
+
+	void Awake()
+	{
+		offset = transform.position - target.position;
+	}
+
+	void Update()
+	{
+		transform.position = target.position + offset;
+	}
+}
