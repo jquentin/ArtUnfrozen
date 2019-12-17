9c632a625ddff839d42222276053256ef95bcfbf
Jeremy Quentin <jeremy.quentin@gmail.com>
2019-12-17 01:01:18 +0100

    Le Parc : Use particles for trails

--- Le Parc/Assets/BallGroup.cs
+++ Le Parc/Assets/BallGroup.cs
@@ -8,13 +8,31 @@ public class BallGroup : MonoBehaviour
 {
 	[SerializeField] float speed = 1f;
 
+	int i;
+
+	void Start()
+	{
+		foreach (TrailRenderer trail in GetComponentsInChildren<TrailRenderer>())
+		{
+			trail.enabled = false;
+		}
+
+	}
+
 	void Update()
 	{
 		transform.position += new Vector3(0f, Time.deltaTime * speed, 0f);
+		//foreach(TrailRenderer trail in GetComponentsInChildren<TrailRenderer>())
+		//{
+		//	Instantiate(trail.gameObject, trail.transform.position, trail.transform.rotation);
+		//}
 		List<TrailRenderer> orderedTrails = GetComponentsInChildren<TrailRenderer>().OrderByDescending(r => r.transform.position.y).ToList();
 		for (int i = 0; i < orderedTrails.Count; i++)
 		{
-			orderedTrails[i].sortingOrder = i;
+			//orderedTrails[i].sortingOrder = i;
+			Vector3 pos = orderedTrails[i].transform.position;
+			pos.z = -i * 0.0001f - Time.frameCount * 0.1f;
+			orderedTrails[i].transform.position = pos;
 		}
 		if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
@@ -28,6 +46,7 @@ public class BallGroup : MonoBehaviour
 
 	void Turn(bool left)
 	{
-		transform.DORotate((left ? 1f : -1f) * Vector3.fwd * 45f, 0.5f).SetRelative(true);
+		i = i + (left ? 1 : -1);
+		transform.DORotate(i * Vector3.fwd * 45f, 0.5f);
 	}
 }
